415f5b6be238f521bd1593ed045503aa095af91f
Zavtrax007 <zavtrax007@gmail.com>
2024-05-07 12:34:48 +0300

    Koshka

--- sem_5/task_5.py
+++ sem_5/task_5.py
@@ -2,12 +2,14 @@ from Tree_al import Node, dept_traversal
 
 
 def isSameTree(a, b):
-    if type(a) is not Node or type(b) is not Node:
-        raise TypeError('a and b must be Nodes')
     if a is None and b is None:
         return True
     if a is None or b is None:
         return False
+    if type(a) is not Node and type(a) is not True and type(a) is not False:
+        raise TypeError('a and b must be Nodes')
+    if type(b) is not Node and type(b) is not True and type(b) is not False:
+        raise TypeError('a and b must be Nodes')
     if a.data != b.data:
         return False
     return isSameTree(a.left, b.left) and isSameTree(a.right, b.right)
--- sem_5/test_5.py
+++ sem_5/test_5.py
@@ -3,15 +3,17 @@ from task_1 import buildTree
 from task_5 import isSameTree
 from Tree_al import Node, dept_traversal
 
+a = [7, 4, 5, 1, 8, 11, 17]
+b = [7, 4, 5, 1, 8, 11, 17]
+c = [7, 4, 5, 18, 8, 11, 17]
+d = [7, 4, 3]
+
 
 class TestIsSameTree(unittest.TestCase):
     def testArea(self):
-        self.assertEqual(isSameTree([10, 6, 13, 3, 7, 12, 14]), 42)
-        self.assertEqual(isSameTree([10, 6, 13, 3, 7, 12]), 39)
-        self.assertEqual(isSameTree([10, 6, 13, 4, 7, 12, 20]), 80)
-        self.assertEqual(isSameTree([10]), 100)
-        self.assertEqual(isSameTree([10, 2]), 20)
-        self.assertEqual(isSameTree([]), None)
+        self.assertEqual(isSameTree(buildTree(a, 0), buildTree(b, 0)), True)
+        self.assertEqual(isSameTree(buildTree(a, 0), buildTree(c, 0)), False)
+        self.assertEqual(isSameTree(buildTree(a, 0), buildTree(d, 0)), False)
 
     def test_types(self):
         self.assertRaises(TypeError, isSameTree, 1.5, 7)
